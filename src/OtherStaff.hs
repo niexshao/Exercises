6a0779b8f956f86b8966dff589c2ee1e92f79d0d
xi.nie <xi.nie@outlook.com>
2016-10-26 17:25:24 +0800

    Finished problem 68

--- src/OtherStaff.hs
+++ src/OtherStaff.hs
@@ -4,7 +4,7 @@ import Data.List
 import Data.Char
 import Data.Array
 import Data.Scientific (fromRationalRepetend, formatScientific, FPFormat(..))
-import Data.Ratio ((%))
+import Data.Ratio
 import Data.Function (on)
 import Numeric (showIntAtBase)
 
@@ -42,7 +42,7 @@ p46 = [n | n <- [3,5..]
               where isprime n =
                       let limitn = floor . sqrt . fromIntegral $ n
                           lessThann = takeWhile (<= limitn) primes
-                      in all (not . (==0) . (n `rem`)) lessThann
+                      in all ((/=0) . (n `rem`)) lessThann
             test y x = let minus = y - x
                            delta = sqrt . fromIntegral . (`div` 2) $ minus
                        in (even minus) && ((floor delta)^2 == (div minus 2))
@@ -110,3 +110,72 @@ p53 = length . filter (>1000000). concatMap combine $ [1..100]
 
 data Card =
   Card Int | Jack | Queen | King | Ace deriving (Eq, Ord, Show)
+
+-- problem 64
+p64 :: Int -> Int
+p64 num = 
+  length . filter snd . map f $ [1..num]
+  where 
+    gen s1 =
+      let generate now@(n, a, b) ns old = 
+            let w = (n-a^2) `div` b
+                k = div (a+floor (sqrt (fromIntegral n))) w
+            in if now `elem` old
+                 then (ns, old)
+                 else generate (n, k*w-a, w) (k:ns) (now:old)
+      in generate s1 [] []
+    periodSquare n =
+      let n' = floor . sqrt . fromIntegral $ n
+          (left, _) = gen (n, n',1)
+      in if n'^2 == n
+           then (n', [])
+           else (n', left)
+    f n = let (a, b) = periodSquare n
+          in (a, odd . length $ b)
+
+-- problem 65
+p65 :: Int -> Int
+p65 n = sum . map (\n -> read [n]) . show . denominator . compose . take n $ es
+  where 
+    es = 2:1:2:(concat [[1,1,2*k] | k <- [2..]])
+    compose :: [Ratio Integer] -> Ratio Integer -- this is necessary, because int won't be enough to store the data
+    compose = foldr (\x acc -> 1 / (x + acc)) 0
+
+-- problem 66
+solve :: Integer -> (Integer, Integer)
+solve d =
+  -- given d, output (x, y)
+  head [(x, y) | y <- [1..], 
+                 let delta = (d*y^2 + 1),
+                 let x = floor . sqrt . fromIntegral $ delta,
+                 x^2 == delta]
+{-p66 :: Int -> [(Int, Int, Int)]-}
+{-p66 num = sort . map de $ ns -}
+{-  where ns = [n | n <- [1..num], (floor . sqrt . fromIntegral $ n)^2 /= n]-}
+{-        de d = let (x, y) = solve d in (x, y, d) -}
+{-        take3 (a, b, c) = c-}
+
+-- problem 69
+p68 :: Integer
+p68 = maximum . filter ((==16) . length . show) . fmap convert . filter check . fmap reform . permutations $ [1..10]
+  where  reform xs = 
+           let (a, b) = splitAt 5 xs
+           in rearange $ zip3' a b (tail b ++ [head b]) -- trick here, clockwise and starting from minimum
+         zip3' (x:xs) (y:ys) (z:zs) = [x,y,z] : zip3' xs ys zs
+         zip3' _ _ _ = []
+         rearange xs = 
+           let small = minimum . fmap head $ xs
+               (a, b) = span ((/= small) . head) xs
+           in b ++ a
+         check ys =
+           let y : yy = fmap sum ys
+           in all (== y) yy
+         convert = read . concatMap show . concat 
+
+-- problem 69
+
+phi :: Integer -> Int
+phi n = length . filter ((==1) . (gcd n)) $ [1..n-1]
+
+p69 n = 
+  map (\n -> (fromIntegral n) / (fromIntegral (phi n))) [2..n]
